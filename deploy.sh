1573183fa01a0c723a06af234ffb9e161d0f4bb8
Techaso <rkkasotiya@gmail.com>
2022-01-20 22:29:30 +0530

    Edited for deployement on github

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:Techaso/todo-vueapp.git main
+git push -f git@github.com:Techaso/todo-vueapp.git master:gh-pages
 cd -
